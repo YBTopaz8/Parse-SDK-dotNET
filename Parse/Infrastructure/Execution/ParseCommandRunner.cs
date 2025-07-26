52112671c9de0a0227621914fdd7b7d3f650a6b7
Yvan Brunel <8brunel@gmail.com>
2025-07-26 03:20:50 -0400

    Enhance ParseCommandRunner error handling and JSON support
    Added new using directives for JSON serialization and Parse namespaces. Improved handling of HTTP response codes, specifically for 100, 201, 400, 401, 403, and 404. Implemented deserialization of error payloads for 400 responses, with appropriate exception handling. Updated 404 response to include more context in the exception message. Retained comment on the removal of the installationFetchTask variable due to async/await simplifications.

--- Parse/Infrastructure/Execution/ParseCommandRunner.cs
+++ Parse/Infrastructure/Execution/ParseCommandRunner.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Net;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
+
 using Parse.Abstractions.Infrastructure;
 using Parse.Abstractions.Infrastructure.Execution;
 using Parse.Abstractions.Platform.Installations;
@@ -75,13 +77,49 @@ public class ParseCommandRunner : IParseCommandRunner
         var responseCode = (int) statusCode;
 
 
+        if (responseCode <= 100 && responseCode <200)
+        {
+
+        }
         if (responseCode == 200)
         {
-            
+
+        }
+        else if (responseCode == 201)
+        {
+            // Created
+        }
+        else if (responseCode == 400)
+        {
+            ParseErrorPayload payload = null;
+            try
+            {
+                payload = JsonSerializer.Deserialize<ParseErrorPayload>(content);
+            }
+            catch (JsonException)
+            {
+                throw new ParseFailureException(
+                    ParseFailureException.ErrorCode.BadRequest,
+                    "Bad Request: unable to parse error payload");
+            }
+
+            ParseFailureException.ErrorCode code = (ParseFailureException.ErrorCode) payload.code;
+
+
+            throw new ParseFailureException(code: code, payload.error);
+
+        }
+        else if (responseCode == 401)
+        {
+            throw new ParseFailureException(ParseFailureException.ErrorCode.ObjectNotFound, content);
+        }
+        else if (responseCode == 403)
+        {
+            throw new ParseFailureException(ParseFailureException.ErrorCode.OperationForbidden, content);
         }
         else if (responseCode == 404)
         {
-            throw new ParseFailureException(ParseFailureException.ErrorCode.ERROR404, "Error 404");
+            throw new ParseFailureException(ParseFailureException.ErrorCode.ERROR404, "Error 404 " +response.Item1+ response.Item2);
         }
         if (responseCode == 410)
         {
@@ -198,7 +236,6 @@ public class ParseCommandRunner : IParseCommandRunner
 
         return newCommand;
 
-        //by the way, The original installationFetchTask variable was removed, as the async/await pattern eliminates the need for it.
     }
 
 }
