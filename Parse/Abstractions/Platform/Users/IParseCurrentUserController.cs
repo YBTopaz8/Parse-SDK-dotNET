9af43a3e743f3602d50b164f837b251ea07fef5f
Yvan Brunel <8brunel@gmail.com>
2025-07-26 00:38:47 -0400

    Add CurrentUser property to IParseCurrentUserController
    Updated the IParseCurrentUserController interface to include a new CurrentUser property that returns a ParseUser object. This enhancement allows direct access to the current user while keeping existing method signatures unchanged.

--- Parse/Abstractions/Platform/Users/IParseCurrentUserController.cs
+++ Parse/Abstractions/Platform/Users/IParseCurrentUserController.cs
@@ -7,6 +7,7 @@ namespace Parse.Abstractions.Platform.Users;
 
 public interface IParseCurrentUserController : IParseObjectCurrentController<ParseUser>
 {
+    ParseUser CurrentUser { get; }
     Task<string> GetCurrentSessionTokenAsync(IServiceHub serviceHub, CancellationToken cancellationToken = default);
 
     Task LogOutAsync(IServiceHub serviceHub, CancellationToken cancellationToken = default);
